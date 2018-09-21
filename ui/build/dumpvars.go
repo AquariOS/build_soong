31f61077c212d6784348a36b69c17de7fb2a5a1d
calebcabob <calphonic@gmail.com>
2018-09-20 20:59:00 -0400

    Cleanup banner and add some color...just for fun
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -123,28 +123,28 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 
 // Variables to print out in the top banner
 var BannerVars = []string{
-	"PLATFORM_VERSION_CODENAME",
+//	"PLATFORM_VERSION_CODENAME",
     "AQUARIOS_VERSION",
-	"PLATFORM_VERSION",
+//	"PLATFORM_VERSION",
 	"TARGET_PRODUCT",
 	"TARGET_BUILD_VARIANT",
-	"TARGET_BUILD_TYPE",
-    "TARGET_PLATFORM_VERSION",
+//	"TARGET_BUILD_TYPE",
+//  "TARGET_PLATFORM_VERSION",
 	"TARGET_BUILD_APPS",
-	"TARGET_ARCH",
-	"TARGET_ARCH_VARIANT",
-	"TARGET_CPU_VARIANT",
+//	"TARGET_ARCH",
+//	"TARGET_ARCH_VARIANT",
+//	"TARGET_CPU_VARIANT",
 	"TARGET_2ND_ARCH",
 	"TARGET_2ND_ARCH_VARIANT",
 	"TARGET_2ND_CPU_VARIANT",
-	"HOST_ARCH",
-	"HOST_2ND_ARCH",
+//	"HOST_ARCH",
+//	"HOST_2ND_ARCH",
 	"HOST_OS",
-	"HOST_OS_EXTRA",
-	"HOST_CROSS_OS",
-	"HOST_CROSS_ARCH",
-	"HOST_CROSS_2ND_ARCH",
-	"HOST_BUILD_TYPE",
+//	"HOST_OS_EXTRA",
+//	"HOST_CROSS_OS",
+//	"HOST_CROSS_ARCH",
+//	"HOST_CROSS_2ND_ARCH",
+//	"HOST_BUILD_TYPE",
 	"BUILD_ID",
 	"OUT_DIR",
 	"AUX_OS_VARIANT_LIST",
@@ -156,13 +156,18 @@ var BannerVars = []string{
 func Banner(make_vars map[string]string) string {
 	b := &bytes.Buffer{}
 
-	fmt.Fprintln(b, "============================================")
+	fmt.Fprintln(b, "=============================================")
+	fmt.Fprintln(b, "≪  🐬 ⚯  🐙 ⚯  🐟 ⚯  🐬 ⚯  🐟 ⚯  🐙 ⚯  🐬  ≫")
+	fmt.Fprintln(b, "=============================================")
 	for _, name := range BannerVars {
 		if make_vars[name] != "" {
 			fmt.Fprintf(b, "%s=%s\n", name, make_vars[name])
 		}
 	}
-	fmt.Fprint(b, "============================================")
+
+	fmt.Fprintln(b, "=============================================")
+	fmt.Fprintln(b, "≪  🐬 ⚯  🐙 ⚯  🐟 ⚯  🐬 ⚯  🐟 ⚯  🐙 ⚯  🐬  ≫")
+	fmt.Fprintln(b, "=============================================")
 
 	return b.String()
 }
