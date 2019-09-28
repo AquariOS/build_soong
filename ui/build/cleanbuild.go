LineageOS/android_build_soong

cleanbuild: add *.zip.md5sum to installclean_files
Change-Id: I9af8579f4acf5043375650ec4f7fadb66c0eb15e

ui/build/cleanbuild.go
@@ -96,6 +96,7 @@ func installClean(ctx Context, config Config, what int) {
 		hostOut("vts"),
 		productOut("*.img"),
 		productOut("*.zip"),
+		productOut("*.zip.md5"),
 		productOut("android-info.txt"),
 		productOut("kernel"),
 		productOut("data"),

AquariOS/build_soong

ui/build/cleanbuild.go
@@ -98,6 +98,7 @@ func installClean(ctx Context, config Config, what int) {
 		hostOut("vts"),
 		productOut("*.img"),
 		productOut("*.zip"),
+		productOut("*.zip.md5sum"),
 		productOut("android-info.txt"),
 		productOut("kernel"),
 		productOut("data"),